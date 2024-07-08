40caab11d4f915d03170ab079ca18ecc192fa4f6
Raheem <159470974+ChismanRaheem@users.noreply.github.com>
2024-07-07 17:00:44 -0700

    Add files via upload

---
+++ TodoListService/Data/TodoListServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoListService.Models;
+
+namespace TodoListService.Data
+{
+    public class TodoListServiceContext : DbContext
+    {
+        public TodoListServiceContext (DbContextOptions<TodoListServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoListService.Models.ToDoItem> ToDoItem { get; set; } = default!;
+    }
+}
